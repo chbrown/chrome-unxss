e1e8db7419eb5b4f6a63d5e588d8cfa721dba61d
Christopher Brown <io@henrian.com>
2015-01-10 10:25:16 -0600

    Remove headers before setting them due to new Chrome behavior

--- background.js
+++ background.js
@@ -1,70 +1,88 @@
 /*jslint browser: true */ /* globals chrome */
 
-var responseListener = function(details) {
-  /**
-  Example / typical details:
-    .statusLine: 'HTTP/1.1 200 OK'
-    .method: 'GET'
-    .url: 'http://google.com/'
-    .frameId: 0                     // 0 for the main_frame, 1+ for subframes
-    .parentFrameId: -1              // != 1 for subframes
-    .type: One of:
-      'main_frame',
-      'sub_frame',
-      'stylesheet',
-      'script',
-      'image',
-      'object',
-      'xmlhttprequest',
-      'other'                      // not exactly sure; pre-flight header check?
-    .tabId: 543                    // or -1 if the request is not related to a tab
-    .requestId: "98233"
-    .timeStamp: 1414070119031.556  // milliseconds with decimal nanoseconds
-    .responseHeaders: Array of { name: <Header>, value: <Value> } objects, e.g.:
-      [
-        { name: "Server", value: "nginx" },
-        { name: "Date", value: "Thu, 23 Oct 2014 12:34:56 GMT" },
-        { name: "Content-Type", value: "text/html" }
-      ]
+/** removeMatchingHeaders(headers: Header[], pattern: RegExp): void
 
-  Complete documentation:
-    https://developer.chrome.com/extensions/webRequest#event-onHeadersReceived
-  */
-
-  var active = true;
-  // var active = details.type == 'main_frame';
-  if (active) {
-    // console.log('details.responseHeaders', details.responseHeaders);
-    // for (var i = 0, header; (header = details.responseHeaders[i]); i++) {}
-
-    // localStorage values are always strings
-    if (localStorage.csp == 'true') {
-      // console.log('cleaning: %s, %d headers', details.url, details.responseHeaders.length);
-      // var original_n_headers = details.responseHeaders.length;
-      details.responseHeaders = details.responseHeaders.filter(function(header) {
-        return !header.name.match(/content-security-policy/i);
-      });
-      // console.log('removed %d headers from %s', original_n_headers - details.responseHeaders.length, details.url);
+Remove the first item from `headers` with a .name property matching `pattern`.
+Modifies the Array in-place. Returns after the first match.
+*/
+function removeMatchingHeaders(headers, regex) {
+  for (var i = 0, header; (header = headers[i]); i++) {
+    if (header.name.match(regex)) {
+      headers.splice(i, 1);
+      log('Removing header "' + header.name + '":"' + header.value + '"');
+      return;
     }
-    if (localStorage.origin == 'true') {
-      // hopefully re-setting the header will overwrite the previous one, if any?
+  }
+}
+
+function log(message) {
+  if (localStorage.log == 'true') {
+    console.log(message);
+  }
+}
+
+/**
+Example / typical details:
+  .statusLine: 'HTTP/1.1 200 OK'
+  .method: 'GET'
+  .url: 'http://google.com/'
+  .frameId: 0                     // 0 for the main_frame, 1+ for subframes
+  .parentFrameId: -1              // != 1 for subframes
+  .type: One of:
+    'main_frame',
+    'sub_frame',
+    'stylesheet',
+    'script',
+    'image',
+    'object',
+    'xmlhttprequest',
+    'other'                      // not exactly sure; pre-flight header check?
+  .tabId: 543                    // or -1 if the request is not related to a tab
+  .requestId: "98233"
+  .timeStamp: 1414070119031.556  // milliseconds with decimal nanoseconds
+  .responseHeaders: Array of { name: <Header>, value: <Value> } objects, e.g.:
+    [
+      { name: "Server", value: "nginx" },
+      { name: "Date", value: "Thu, 23 Oct 2014 12:34:56 GMT" },
+      { name: "Content-Type", value: "text/html" }
+    ]
+
+In some versions of Chrome, simply adding a new header combines its value with
+the existing header. To avoid conflicts and improper header values, we always
+remove matching headers before setting a new value.
+
+Complete documentation:
+  https://developer.chrome.com/extensions/webRequest#event-onHeadersReceived
+*/
+function responseListener(details) {
+  // localStorage values are always strings
+  var strip_csp = localStorage.csp == 'true';
+  var allow_origin_star = localStorage.origin == 'true';
+  var allow_methods_star = localStorage.methods == 'true';
+  var strip_frame_options = localStorage.frame_options == 'true';
+
+  var active = strip_csp || allow_origin_star || allow_methods_star || strip_frame_options;
+  // var active = active && (details.type == 'main_frame');
+  if (active) {
+    log('Removing headers where applicable');
+    if (strip_csp) {
+      removeMatchingHeaders(details.responseHeaders, /content-security-policy/i);
+    }
+    if (allow_origin_star) {
+      removeMatchingHeaders(details.responseHeaders, /access-control-allow-origin/i);
       details.responseHeaders.push({name: 'Access-Control-Allow-Origin', value: '*'});
     }
-    if (localStorage.methods == 'true') {
+    if (allow_methods_star) {
+      removeMatchingHeaders(details.responseHeaders, /access-control-allow-methods/i);
       details.responseHeaders.push({name: 'Access-Control-Allow-Methods', value: '*'});
-      // console.log('added Access-Control-Allow-Methods: * header to %s', details.url);
     }
-    if (localStorage.frame_options == 'true') {
-      // console.log('localStorage.frame_options is true; found %d headers', details.responseHeaders.length);
-      details.responseHeaders = details.responseHeaders.filter(function(header) {
-        return !header.name.match(/x-frame-options/i);
-      });
-      // console.log('found %d headers', details.responseHeaders.length);
+    if (strip_frame_options) {
+      removeMatchingHeaders(details.responseHeaders, /x-frame-options/i);
     }
   }
 
   return {responseHeaders: details.responseHeaders};
-};
+}
 
 chrome.webRequest.onHeadersReceived.addListener(responseListener, {
   urls: ['*://*/*']
